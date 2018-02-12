56bcd06368107ccb4937b66145617dd352e51b4b
Trevor Old <trevor.old@outlook.com>
2018-02-12 11:35:24 -0800

    Implemented tictactoe board and js / css connections

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,6 @@
-
-
+ document.addEventListener('DOMContentLoaded', function(){ 
+    // your code goes here
+    var board = document.getElementById('board');
+    console.log(board);
+}, false);
+ 
