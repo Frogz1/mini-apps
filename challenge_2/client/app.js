ff8937b481e0fd433568c3672de991881d4f5b38
Trevor Old <trevor.old@outlook.com>
2018-02-13 15:21:10 -0800

    Implemented AJAX calls and display of CSV data to page

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,33 @@
+$(document).ready(function() {
+
+  var requestBody = '';
+  var jsonInput = document.getElementById("json-input");
+    var trackInput = () => {
+      requestBody = jsonInput.value;  
+      console.log(requestBody)
+    }
+  jsonInput.addEventListener('keydown', trackInput);
+  $("#submit").on("click", function(e) {
+    e.preventDefault();
+      $.ajax({
+        "async":true,
+        "crossDomain": true,
+        type: "POST",
+        url: "http://localhost:3001/generate",
+        data:$("#json-input").val(),
+        headers:{
+          "Content-Type":"application/json"
+        },
+        // dataType: "application/json",
+        success: function (response) {
+          console.log(response);
+          $("#result").append("<p>" + response + "</p>");      
+        }
+      });
+    })
+  
+  
+})
 
 
+    // Now use it!
