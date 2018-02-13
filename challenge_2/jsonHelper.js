3130893c326ad1fb8583b1b313c8d636b9336793
Trevor Old <trevor.old@outlook.com>
2018-02-13 12:14:10 -0800

    JSONHelper correctly un-nesting children into response

--- challenge_2/jsonHelper.js
+++ challenge_2/jsonHelper.js
@@ -1,17 +1,20 @@
-var handleKeys = (json) => {
-  var keys = Object.keys(json);
+var fs = require('fs');
+var Promise = require('bluebird');
+Promise.promisifyAll(fs);
 
-};
 
-var handleChildren = (json) => {
+
+exports.HandleChildren = (json) => {
   var values = [];
-  values.push(json);
+  // values.push(json);
+
   var recurse = function (node) {
     if (!node.children) {
       return;
     } else {
+      values.push(node);
       for (var i = 0; i < node.children.length; i++) {
-        values.push(node.children[i]);
+        
         recurse(node.children[i]);
       }
     }
@@ -24,54 +27,3 @@ var handleChildren = (json) => {
   });
   return values;
 }
-
-var sample = {
-  "firstName": "Joshie",
-  "lastName": "Wyattson",
-  "county": "San Mateo",
-  "city": "San Mateo",
-  "role": "Broker",
-  "sales": 1000000,
-  "children": [
-    {
-      "firstName": "Beth Jr.",
-      "lastName": "Johnson",
-      "county": "San Mateo",
-      "city": "Pacifica",
-      "role": "Manager",
-      "sales": 2900000,
-      "children": [
-        {
-          "firstName": "Smitty",
-          "lastName": "Won",
-          "county": "San Mateo",
-          "city": "Redwood City",
-          "role": "Sales Person",
-          "sales": 4800000,
-          "children": []
-        },
-        {
-          "firstName": "Allen",
-          "lastName": "Price",
-          "county": "San Mateo",
-          "city": "Burlingame",
-          "role": "Sales Person",
-          "sales": 2500000,
-          "children": []
-        }
-      ]
-    },
-    {
-      "firstName": "Beth",
-      "lastName": "Johnson",
-      "county": "San Francisco",
-      "city": "San Francisco",
-      "role": "Broker/Sales Person",
-      "sales": 7500000,
-      "children": []
-    }
-  ]
-}
-console.table(handleChildren(sample));
-
-
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,41 +2,45 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser')
 var app = express();
+var Promise = require('bluebird')
 var fs = require('fs');
+Promise.promisifyAll(fs);
+var helper = require('./jsonHelper');
+
+app.use(express.static(path.join(__dirname, '/client')));
 var jsonParser = bodyParser.json();
 
-
-app.use(express.static(path.join(__dirname,'/client')));
-
 app.post('/generate', jsonParser, (req, res) => {
+  // console.log(req.body);
+  // console.log(helper.HandleChildren(req.body))
+  // var values = helper.HandleChildren(req.body);
+  // console.log(values);
 
-  fs.writeFile('sample.json', JSON.stringify(req.body,null, 2), (err, data) => {
-    if (err) {
-        console.log(`err writing file with ${err}`)
-    } else {
-        console.log(`${data} saved`);
-    }
-  })
-    
-    res.send('OK');
+
+  
+  
+  res.send(helper.HandleChildren(req.body));
 })
 
 app.listen(3001, () => {
-    console.log(`Listening on port 3001`)
+  console.log(`Listening on port 3001`)
 })
 
+// const items = req.body;     const replacer = (key, value) => value === null ?
+// '' : value // specify how you want to handle null values here     const
+// header = Object.keys(items)     let csv = items.map(row =>
+// header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+//    csv.unshift(header.join(','))     csv = csv.join('\r\n') console.log(csv)
 
 
+// fs.writeFileAsync('sample.json', JSON.stringify(req.body, null, 2), (err, data) =>{
+//   if (err) {
+//     console.log(`err writing file with ${err}`)
+//     reject(err);
+//   } else if (data) {
 
-
-
-
-
-// const items = req.body;
-//     const replacer = (key, value) => value === null ? '' : value // specify how you want to handle null values here
-//     const header = Object.keys(items)
-//     let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-//     csv.unshift(header.join(','))
-//     csv = csv.join('\r\n')
-
-// console.log(csv)
+//     console.log(`${data} saved`);
+//   }
+    
+  
+// })
